5cc1fa0b99aa1a047dbe2c2579185a639c37f683
mboude <docker47@gmail.com>
2022-02-02 21:51:23 +0100

    DeviceExample initial commit

---
+++ examples/DeviceExample/DeviceExample/Program.cs
@@ -0,0 +1,124 @@
+using System;
+using System.Diagnostics;
+using System.IO.Ports;
+using System.Text;
+using nanoFramework.Hardware.Esp32;
+using TinyGPSPlusNF;
+
+namespace DeviceExample
+{
+    public class Program
+    {
+        private static TinyGPSPlus s_gps;
+        private static SerialPort s_serial;
+
+        public static void Main()
+        {
+            s_gps = new();
+
+            // Example based on an ESP32 board (NodeMCU-32S ESP-WROOM-32) which requires pin configuration.
+            Configuration.SetPinFunction(Gpio.IO05, DeviceFunction.COM3_RX);
+            Configuration.SetPinFunction(Gpio.IO04, DeviceFunction.COM3_TX);
+
+            // Example based on a NEO-6M module (GY-NEO6MV2) which operates at 9600 bauds by default.
+            s_serial = new SerialPort("COM3", 9600)
+            {
+                Handshake = Handshake.None,
+                ReadTimeout = 10,
+            };
+
+            s_serial.DataReceived += GpsDataReceived;
+            s_serial.Open();
+
+            Debug.WriteLine("DeviceExample");
+            Debug.WriteLine("A simple demonstration of TinyGPSPlus with an attached GPS module");
+            Debug.Write("Testing TinyGPSPlus library v");
+            Debug.WriteLine(TinyGPSPlus.LibraryVersion);
+            Debug.WriteLine(string.Empty);
+
+            while (true)
+            {
+                // Now we wait
+            }
+        }
+
+        private static void GpsDataReceived(object sender, SerialDataReceivedEventArgs e)
+        {
+            if (s_serial.BytesToRead == 0)
+            {
+                return;
+            }
+
+            string temp;
+
+            try
+            {
+                byte[] buffer = new byte[s_serial.BytesToRead];
+                int bytesRead = s_serial.Read(buffer, 0, buffer.Length);
+                temp = Encoding.UTF8.GetString(buffer, 0, bytesRead);
+            }
+            catch (Exception)
+            {
+                return;
+            }
+
+            // We can receive more than one sentence at a time
+            string[] nmea = temp.Split('\n');
+
+            for (int i = 0; i < nmea.Length && !string.IsNullOrEmpty(nmea[i]); i++)
+            {
+                if (s_gps.Encode(nmea[i]))
+                {
+                    DisplayInfo();
+                }
+            }
+        }
+
+        private static void DisplayInfo()
+        {
+            Debug.Write("Location: ");
+            if (s_gps.Location.IsValid)
+            {
+                Debug.Write(s_gps.Location.Latitude.Degrees.ToString());
+                Debug.Write(",");
+                Debug.Write(s_gps.Location.Longitude.Degrees.ToString());
+            }
+            else
+            {
+                Debug.Write("INVALID");
+            }
+
+            Debug.Write("  Date/Time: ");
+            if (s_gps.Date.IsValid)
+            {
+                Debug.Write(s_gps.Date.Year.ToString());
+                Debug.Write("/");
+                Debug.Write(s_gps.Date.Month.ToString("D2"));
+                Debug.Write("/");
+                Debug.Write(s_gps.Date.Day.ToString("D2"));
+            }
+            else
+            {
+                Debug.Write("INVALID");
+            }
+
+            Debug.Write(" ");
+            if (s_gps.Time.IsValid)
+            {
+                Debug.Write(s_gps.Time.Hour.ToString("D2"));
+                Debug.Write(":");
+                Debug.Write(s_gps.Time.Minute.ToString("D2"));
+                Debug.Write(":");
+                Debug.Write(s_gps.Time.Second.ToString("D2"));
+                Debug.Write(".");
+                Debug.Write(s_gps.Time.Centisecond.ToString("D2"));
+            }
+            else
+            {
+                Debug.Write("INVALID");
+            }
+
+            Debug.WriteLine(string.Empty);
+        }
+    }
+}
---
+++ examples/DeviceExample/README.md
@@ -0,0 +1,32 @@
+# TinyGPSPlusNF - DeviceExample
+Requires a gps device.
+
+Example based on a NEO-6M module (GY-NEO6MV2) and an ESP32 board (NodeMCU-32S ESP-WROOM-32).
+
+Expected debug output:
+```
+DeviceExample
+A simple demonstration of TinyGPSPlus with an attached GPS module
+Testing TinyGPSPlus library v1.0.0.0
+
+Location: 47.216127,-1.549329  Date/Time: 2022/02/02 20:43:57.00
+Location: 47.216127,-1.549329  Date/Time: 2022/02/02 20:43:57.00
+Location: 47.216127,-1.549329  Date/Time: 2022/02/02 20:43:57.00
+Location: 47.216127,-1.549329  Date/Time: 2022/02/02 20:43:57.00
+Location: 47.216127,-1.549329  Date/Time: 2022/02/02 20:43:57.00
+Location: 47.216127,-1.549329  Date/Time: 2022/02/02 20:43:57.00
+Location: 47.216127,-1.549329  Date/Time: 2022/02/02 20:43:57.00
+Location: 47.216127,-1.549329  Date/Time: 2022/02/02 20:43:57.00
+Location: 47.216127,-1.549329  Date/Time: 2022/02/02 20:43:57.00
+Location: 47.216127,-1.549326  Date/Time: 2022/02/02 20:43:58.00
+Location: 47.216127,-1.549326  Date/Time: 2022/02/02 20:43:58.00
+Location: 47.216127,-1.549326  Date/Time: 2022/02/02 20:43:58.00
+Location: 47.216127,-1.549326  Date/Time: 2022/02/02 20:43:58.00
+Location: 47.216127,-1.549326  Date/Time: 2022/02/02 20:43:58.00
+Location: 47.216127,-1.549326  Date/Time: 2022/02/02 20:43:58.00
+Location: 47.216127,-1.549326  Date/Time: 2022/02/02 20:43:58.00
+Location: 47.216127,-1.549326  Date/Time: 2022/02/02 20:43:58.00
+[...]
+
+Done.
+```
